c5e42ef10115606668a911e8fb447649e35404db
Tianzong Cheng <oierprime@outlook.com>
2024-10-18 16:38:37 +0800

    feat: separate waypoints according to distance

--- planning/pick_up_example.py
+++ planning/pick_up_example.py
@@ -18,6 +18,8 @@ args, unknown = parser.parse_known_args()
 
 my_world = World(stage_units_in_meters=1.0)
 
+# ----------
+
 target_position = [
     np.array([0.4, -0.25, 0]),
     np.array([0.5, -0.3, 0]),
@@ -49,6 +51,8 @@ my_task = PickPlace(
     cube_size=cube_size,
 )
 
+# ----------
+
 my_world.add_task(my_task)
 my_world.reset()
 
@@ -58,7 +62,7 @@ my_controller = PickPlaceController(
 )
 task_params = my_world.get_task("denso_pick_place").get_params()
 articulation_controller = my_denso.get_articulation_controller()
-
+last_end_effector_position = np.array([0, 0, 0])
 i = 0
 action = -1
 start_time = time.time()
@@ -84,6 +88,29 @@ while simulation_app.is_running():
             end_effector_offset=np.array([0.0, 0, 0.18]),
         )
 
+        num = my_task.task_num
+        text = (
+            "./standalone_examples/api/omni.isaac.manipulators/RIZON4/simulate_datasets/order"
+            + str(num)
+            + ".txt"
+        )
+        end_effector_position = observations[task_params["robot_name"]["value"]][
+            "end_effector_position"
+        ]
+
+        if (
+            not last_end_effector_position.any()
+            or np.linalg.norm(last_end_effector_position - end_effector_position) > 0.06
+            or action < my_controller._event
+        ):
+            with open(text, "a", encoding="utf-8") as file:
+                local_action = str(actions)
+                local_action = local_action.replace("'", '"')
+                file.write(local_action + "\n")
+                # file.write(str(my_controller._event) + "\n")
+                print(end_effector_position)
+                last_end_effector_position = end_effector_position
+
         if action < my_controller._event:
             end_time = time.time()
             print(
@@ -91,18 +118,6 @@ while simulation_app.is_running():
             )
             action = my_controller._event
             start_time = time.time()
-        num = my_task.task_num
-        text = (
-            "./standalone_examples/api/omni.isaac.manipulators/RIZON4/simulate_datasets/order"
-            + str(num)
-            + ".txt"
-        )
-
-        os.makedirs(os.path.dirname(text), exist_ok=True)
-        with open(text, "a", encoding="utf-8") as file:
-            local_action = str(actions)
-            local_action = local_action.replace("'", '"')
-            file.write(local_action + "\n")
 
         if my_controller.is_done():
             print("done picking and placing")
@@ -110,12 +125,11 @@ while simulation_app.is_running():
             my_task.task_num += 1
             if my_task.task_num >= my_task._obj_num:
                 exit()
-            print(my_task.task_num)
             task_params = my_world.get_task("denso_pick_place").get_params()
             action = -1
+            last_joint_position = np.array([0, 0, 0])
 
         articulation_controller.apply_action(actions)
-
     if args.test is True:
         break
 

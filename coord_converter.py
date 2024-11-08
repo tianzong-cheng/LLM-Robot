e297adcc121ead3084f54786a8537d8c1abb638c
Tianzong Cheng <oierprime@outlook.com>
2024-11-08 13:01:18 +0800

    feat: use eye-in-hand calibration result

--- coord_converter.py
+++ coord_converter.py
@@ -16,7 +16,24 @@ def transform_object_pose(camera_world_pose, object_camera_pose):
     return object_position_world
 
 
-camera_world_pose = [0.6, -0.2, 0.6, 0.0, 1.0, 0.0, 0.0]  # [x, y, z, qx, qy, qz, qw]
+tcp_pose_matrix = np.array(
+    [[-1, 0, 0, 0.6], [0, 1, 0, -0.2], [0, 0, -1, 0.6], [0, 0, 0, 1]]
+)
+
+transformation_matrix = np.array(
+    [
+        [0.01035566, 0.99726347, 0.07320067, -0.06347553],
+        [-0.99994025, 0.01058405, -0.00273277, 0.1598579],
+        [-0.00350005, -0.073168, 0.99731349, -0.14802994],
+        [0, 0, 0, 1],
+    ]
+)
+
+camera_pose_matrix = tcp_pose_matrix @ transformation_matrix
+
+camera_position = camera_pose_matrix[:3, 3]
+camera_quaternion = R.from_matrix(camera_pose_matrix[:3, :3]).as_quat()
+camera_world_pose = np.concatenate([camera_position, camera_quaternion])
 
 i = 1
 while True:
